9c406396328c573d24a69aced3ca3b59cc901023
Alexander Alexandrov <alexander.rumenov.alexandrov@gmail.com>
2021-12-11 03:06:15 +0200

    Basic project configuration added.

---
+++ GlobalServerSoftware/src/main/java/com/elsys/globalserver/GlobalServerSoftwareApplication.java
@@ -0,0 +1,11 @@
+package com.elsys.globalserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalServerSoftwareApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalServerSoftwareApplication.class, args);
+    }
+}
