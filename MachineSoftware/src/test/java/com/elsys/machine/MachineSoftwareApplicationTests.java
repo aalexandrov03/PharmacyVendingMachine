1e20a19a31690d0f5040390b5579c866db53e843
Alexander Alexandrov <alexander.rumenov.alexandrov@gmail.com>
2021-12-17 14:30:16 +0200

    Added tests to machine project

---
+++ MachineSoftware/src/test/java/com/elsys/machine/MachineSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsys.machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineSoftwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
