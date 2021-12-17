aac0da1e37a6f29cfac2d4cbb2711b51bf81006e
Alexander Alexandrov <alexander.rumenov.alexandrov@gmail.com>
2021-12-17 14:28:29 +0200

    Basic project configuration for machine software created.

---
+++ MachineSoftware/src/main/java/com/elsys/machine/MachineSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.elsys.machine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineSoftwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MachineSoftwareApplication.class, args);
+    }
+
+}
