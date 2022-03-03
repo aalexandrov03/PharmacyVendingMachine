d472d7eeba679e043c831b376cec76c75ea2b41c
alexander <alexander.rumenov.alexandrov@gmail.com>
2022-03-03 14:12:47 +0200

    Added @Repository annotation to data access layer interfaces.

--- GlobalServerSoftware/src/main/java/com/elsys/globalserver/DataAccess/PrescriptionRepository.java
+++ GlobalServerSoftware/src/main/java/com/elsys/globalserver/DataAccess/PrescriptionRepository.java
@@ -2,9 +2,11 @@ package com.elsys.globalserver.DataAccess;
 
 import com.elsys.globalserver.Models.Prescription;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PrescriptionRepository extends CrudRepository<Prescription, Integer> {
     List<Prescription> findByDoctor(String doctor);
     List<Prescription> findByPatient(String patient);
