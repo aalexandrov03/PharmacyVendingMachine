2d62c57d615a3d7f85d98f1b0be060099e9aa17c
alexander <alexander.rumenov.alexandrov@gmail.com>
2022-03-27 20:26:21 +0300

    Added: Unit test for Router code.

--- MachineSoftware/src/main/java/com/elsys/machine/Control/Utils/RouteNode.java
+++ MachineSoftware/src/main/java/com/elsys/machine/Control/Utils/RouteNode.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @AllArgsConstructor
 @Getter
 @Setter
@@ -11,4 +13,17 @@ public class RouteNode {
     private Motor id;
     private long steps;
     private Direction dir;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RouteNode routeNode = (RouteNode) o;
+        return steps == routeNode.steps && id == routeNode.id && dir == routeNode.dir;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, steps, dir);
+    }
 }
